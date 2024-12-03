6a4491fae6f8accdc44ee5ee7d3087f020d218d4
Xavanion <barrettbrown2012@gmail.com>
2024-12-02 18:00:22 -0600

    queries/reports

--- Project_Part_5/Sql_Files/Queries.sql
+++ Project_Part_5/Sql_Files/Queries.sql
@@ -1,4 +1,46 @@
+-- Show Available Books
 SELECT Book.ISBN, Book.title, Book.author
 FROM Book
 JOIN Book_Copy ON Book.ISBN = Book_Copy.ISBN
 WHERE Book_Copy.status = 'Available';
+
+
+-- Show Available Digital Media
+SELECT digital_media_id, title, author FROM digitalmedia WHERE availability_status = 'Available';
+
+
+-- Show Available Magazines
+SELECT magazine_id, issue_number, title, publisher FROM magazine WHERE availability_status = 'Available';
+
+
+-- Find total Number of items loaned out by each membership type
+
+
+
+-- List all overdue items/late fees
+
+
+
+-- List Reserved but not loaned out items
+
+
+
+-- List most popular items by number of loans
+
+
+
+-- Generate a report of total fees collected within the last month, broken down by membership type.
+
+
+
+-- Produce a list of clients who have exceeded their borrowing limits.
+
+
+
+-- Determine the most frequently borrowed items by each client type
+
+
+
+-- Find out which clients have never returned an item late
+
+
---
+++ Project_Part_5/Sql_Files/Reports.sql
@@ -0,0 +1,21 @@
+-- Monthly Sumamry Report
+-- Generate a report summarizing the total number of items loaned, total fees collected, and most popular items for the month.
+-- Breakdown the statistics by client type and item category (Books, Digital Media, Magazines)
+
+
+-- Client Activity Report
+-- Produce an individual report for each client showing their borrowing history, outstanding fees, and any reserved items
+
+
+-- Inventory Report
+-- List all items, their current availability status, and their last borrowed date.
+-- Highlight items that have not been borrowed in the past six months.
+
+
+
+-- Overdue Item Report
+-- Generate a report listing all overdue items, the client responsible, and the calculated late fees.
+
+
+-- Financial Report
+-- Summarize the library’s revenue from fees, showing the breakdown by membership type and item category
