6c06d3958cd98c79d5fce2cc73795694d0874cc9
Xavanion <barrettbrown2012@gmail.com>
2024-12-03 23:35:19 -0600

    Small change

--- Project_Part_5/Interface/main.py
+++ Project_Part_5/Interface/main.py
@@ -55,12 +55,9 @@ def staffInterface(choice):
                 client_id = input('Client ID: ').strip()
                 print('Enter the ISBN of the book to check out:')
                 isbn = input('ISBN: ').strip()
-                print('Enter the Borrow Date of the book to check out:')
+                print('Enter the Borrow Date of the book to check out (YYYY-MM-DD):')
                 borrow_date = input('Borrow Date: ').strip()
-               
-                
 
-                
                 # Check membership type and borrowing limit
                 sql_limit_check = """
                 SELECT COUNT(*) 
@@ -262,7 +259,7 @@ def clientInterface(choice):
             myresult = mycursor.fetchall()
             for x in myresult:
                 print(x)
-    elif choice == 1: # Reserve items
+    elif choice == 1: # Reserve itemsa
         print('Select the # of what to reserve\
               \n\t1) Books\
               \n\t2) Magazines\
