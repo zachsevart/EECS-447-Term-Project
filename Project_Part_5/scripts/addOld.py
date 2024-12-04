2b81c6ad9fff846b7a1d73caf510055712b7eeae
Anorak2 <adamcoleberry@gmail.com>
2024-12-03 21:33:38 -0600

    Added data for previous magazines

--- Project_Part_5/scripts/addOld.py
+++ Project_Part_5/scripts/addOld.py
@@ -22,6 +22,26 @@ def rand_book(clients, cpy):
     print(out)
 
 
+def rand_mag(clients, cpy):
+    # Add a random client
+    out = f'{random.choice(clients)},'
+    out += cpy[0]
+    # Generating a random checkout date in a range
+    start = datetime.datetime(2024,6,1,0,0,0)
+    delta = datetime.timedelta(days=random.randint(0,140))
+    out += ","+ (start + delta).strftime("%Y-%m-%d")  
+    checked = start + delta
+
+    # Setting the return window
+    delta += datetime.timedelta(days=50)
+    out += ","+ (start + delta).strftime("%Y-%m-%d")
+
+    # Setting the return date 
+    delta = datetime.timedelta(days=random.randint(0,58))
+    out += ","+ (checked + delta).strftime("%Y-%m-%d")
+
+    print(out)
+
 if __name__ == "__main__":
     clients = []
     with open('../Data/clients.csv', mode ='r')as file:
@@ -29,20 +49,20 @@ if __name__ == "__main__":
         for lines in csvFile:
             clients += [lines[0]]
     clients.pop(0)
-    book_copy = []
-    with open("../Data/book_copy.csv") as file:
+    magazines = []
+    with open("../Data/magazines.csv") as file:
         csvFile = csv.reader(file)
         for lines in csvFile:
-            book_copy.append(lines)
-    book_copy.pop(0)
+            magazines.append(lines)
+    magazines.pop(0)
     books = []
     with open('../Data/books.csv', mode ='r')as file:
         csvFile = csv.reader(file)
         isFirst = True 
         for lines in csvFile:
             books.append(lines)
-    for cpy in book_copy:
+    for mag in magazines:
         num_checkouts = random.choice([0,1,2,3])
         for x in range(num_checkouts):
-            rand_book(clients, cpy)
+            rand_mag(clients, mag)
 
